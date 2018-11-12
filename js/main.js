c98cc1428f973d3f38807a7b028b986e6ea3eca9
namisumida <sumida.nami@gmail.com>
2018-11-11 20:27:46 -0500

    Updating

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var svg = d3.select("#graphic-svg");
 
 // Define margins
-var margin = {top: 20, bottom: 0, left: 60, right: 0};
+var margin = {top: 20, bottom: 0, left: 60, right: 5};
 var w_svg = document.getElementById('graphic-svg').getBoundingClientRect().width; // get width and height based on window size
 var w_btwn_min = w_svg/10;
 var max_circle_r = ((w_svg - (2*w_btwn_min) - margin.left - margin.right)/3)/2; // width of the svg, minus 2 btwn spacing, divide by 3 to get diameter of each circle, divide by 2 to get r
@@ -32,40 +32,6 @@ var rowConverter = function(d) {
     year: parseInt(d.year)
   };
 };
-
-// text wrapping function
-function wrap(text, width) {
-  text.each(function () {
-    var text = d3.select(this),
-        words = text.text().split(/\s+/).reverse(),
-        word,
-        line = [],
-        lineNumber = 0,
-        lineHeight = 1.3, // ems
-        x = text.attr("x"),
-        y = text.attr("y"),
-        dy = 0, //parseFloat(text.attr("dy")),
-        tspan = text.text(null)
-                    .append("tspan")
-                    .attr("x", x)
-                    .attr("y", y)
-                    .attr("dy", dy + "em");
-    while (word = words.pop()) {
-        line.push(word);
-        tspan.text(line.join(" "));
-        if (tspan.node().getComputedTextLength() > width) {
-            line.pop();
-            tspan.text(line.join(" "));
-            line = [word];
-            tspan = text.append("tspan")
-                        .attr("x", x)
-                        .attr("y", y)
-                        .attr("dy", ++lineNumber * lineHeight + dy + "em")
-                        .text(word);
-        }
-    }
-  });
-}; // end wrap function
 
 // Import data
 d3.csv("data/opoints.csv", rowConverter, function(data) {
@@ -129,19 +95,12 @@ d3.csv("data/opoints.csv", rowConverter, function(data) {
                         .attr("cy", function(d) {
                           return margin.top + 20 + max_circle_r + (max_circle_r - (max_circle_r * d.holds_percent));
                         })
-                      //  .attr("cy", margin.top + 20 + max_circle_r)
                         .attr("r", function(d) {
                           return max_circle_r*d.holds_percent;
                         })
-                        .style("fill", "none")
-                        .style("stroke-width", 2)
+                        .style("stroke-width", 4.5)
                         .style('opacity', 0.15)
-                        .transition()
-                        .duration(2000)
-                        .delay(function(d,i) {
-                          return i*30;
-                        })
-                        .style("stroke", color1);
+                        .style("fill", "none");
 
   var hold_circles_avg = svg.selectAll("avg_hold_circles")
                             .data(dataset_avg)
@@ -154,18 +113,12 @@ d3.csv("data/opoints.csv", rowConverter, function(data) {
                             .attr("cy", function(d) {
                               return margin.top + 20 + max_circle_r + (max_circle_r - (max_circle_r * d.holds_percent));
                             })
-                            //.attr("cy", margin.top + 20 + max_circle_r)
                             .attr("r", function(d) {
                               return max_circle_r * d.holds_percent;
                             })
                             .style("fill", "none")
-                            .style("stroke-width", 3.5)
-                            .style("opacity", 0)
-                            .transition()
-                            .delay(1500)
-                            .duration(600)
-                            .style("opacity", 1)
-                            .style("stroke", color1);
+                            .style("stroke-width", 5)
+                            .style("opacity", 0);
 
   var hold_avg_label = svg.append("text")
                           .text("Average conversion rate")
@@ -173,11 +126,7 @@ d3.csv("data/opoints.csv", rowConverter, function(data) {
                           .attr("x", 0)
                           .attr("y", margin.top + 45 + max_circle_r*2)
                           .call(wrap, 80)
-                          .style("fill", "none")
-                          .transition()
-                          .delay(2000)
-                          .duration(1500)
-                          .style('fill', 'black');
+                          .style("fill", "none");
 
   var hold_avg_text = svg.selectAll("hold_avg_text")
                          .data(dataset_avg)
@@ -193,11 +142,7 @@ d3.csv("data/opoints.csv", rowConverter, function(data) {
                          .attr("y", function(d) {
                            return margin.top + 55 + max_circle_r*2;
                          })
-                         .style("fill", "none")
-                         .transition()
-                         .delay(2000)
-                         .duration(1500)
-                         .style('fill', color1);
+                         .style("fill", "none");
 
   // single possession score circles
   var single_circles = svg.selectAll("single_circles")
@@ -222,14 +167,8 @@ d3.csv("data/opoints.csv", rowConverter, function(data) {
                             return max_circle_r * d.singleholds_percent;
                           })
                           .style("fill", "none")
-                          .style("stroke-width", 2)
-                          .style('opacity', 0.15)
-                          .transition()
-                          .duration(2000)
-                          .delay(function(d,i) {
-                            return 2000+i*30;
-                          })
-                          .style("stroke", color2);
+                          .style("stroke-width", 4.5)
+                          .style('opacity', 0.15);
 
   var single_circles_avg = svg.selectAll("avg_single_circles")
                               .data(dataset_avg)
@@ -246,13 +185,8 @@ d3.csv("data/opoints.csv", rowConverter, function(data) {
                                 return max_circle_r * d.singleholds_percent;
                               })
                               .style("fill", "none")
-                              .style("stroke-width", 3.5)
-                              .style("opacity", 0)
-                              .transition()
-                              .delay(3500)
-                              .duration(600)
-                              .style("opacity", 1)
-                              .style("stroke", color2);
+                              .style("stroke-width", 5)
+                              .style("opacity", 0);
 
   var single_avg_label = svg.append("text")
                             .text("Average single-possession score rate")
@@ -260,11 +194,7 @@ d3.csv("data/opoints.csv", rowConverter, function(data) {
                             .attr("x", 0)
                             .attr("y", margin.top + 85 + max_circle_r*2)
                             .call(wrap, 100)
-                            .style("fill", "none")
-                            .transition()
-                            .delay(4000)
-                            .duration(1500)
-                            .style('fill', 'black');
+                            .style("fill", "none");
 
   var single_avg_text = svg.selectAll("single_avg_text")
                          .data(dataset_avg)
@@ -280,12 +210,231 @@ d3.csv("data/opoints.csv", rowConverter, function(data) {
                          .attr("y", function(d) {
                            return margin.top + 100 + max_circle_r*2;
                          })
-                         .style("fill", "none")
-                         .transition()
-                         .delay(4000)
-                         .duration(1500)
-                         .style('fill', color2);
+                         .style("fill", "none");
 
-// On mouseover of circles 
+// On mouseover of circles
+  hold_circles.on("mouseover", function(d) {
 
+    var currentElement = d3.select(this);
+    var currentID = d.game_id;
+
+    // Determine starting x position
+    var xPosition;
+    if (d.division=="mens") {
+      xPosition = margin.left;
+    }
+    else if (d.division=="mixed") {
+      xPosition = margin.left + max_circle_r*2 + w_btwn_min;
+    }
+    else { xPosition = margin.left + max_circle_r*4 + w_btwn_min*2; }
+
+    // Hide averages
+    hold_avg_text.style("fill", "none");
+    single_avg_text.style("fill", "none");
+    hold_avg_label.style("fill", "none");
+    single_avg_label.style("fill", "none");
+    hold_circles_avg.style("opacity", .2);
+    single_circles_avg.style("opacity", .2);
+
+    // Change opacity of mouseover circle
+    currentElement.style("opacity", 1)
+                  .style("stroke-width", 5);
+    single_circles.filter(function(d) {
+                    return d.game_id == currentID;
+                  })
+                  .style("opacity", 1)
+                  .style("stroke-width", 5);
+
+    // Create a tooltip
+    var tooltip = svg.append("g")
+       .attr("id", "tooltip")
+       .attr("transform", "translate(" + xPosition + "," + (margin.top + 55 + max_circle_r*2) + ")")
+       .attr("width", max_circle_r*2)
+    // Add details
+    tooltip.append('text')
+           .attr("x", max_circle_r)
+           .attr("y", 5)
+           .text(function() {
+             return d.year + " " + d.tournament + " " + d.game_type;
+           })
+           .attr("class", "tooltip_text");
+    tooltip.append("text")
+           .attr("x", max_circle_r)
+           .attr("y", 18)
+           .text(function() {
+             return teamName_convert(d.win_team) + " (" + d.score.split("-")[0] + ") vs. " + teamName_convert(d.lose_team) + " (" + d.score.split("-")[1] + ")";
+           })
+           .attr("class", "tooltip_text");
+    tooltip.append("text")
+           .attr("x", max_circle_r)
+           .attr("y", 45)
+           .text(function() {
+             return d3.format(".0%")(d.holds_percent)
+           })
+           .attr("class", "avg_text")
+           .style("fill", color1);
+    tooltip.append("text")
+           .attr("x", max_circle_r)
+           .attr("y", 75)
+           .text(function() {
+             return d3.format(".0%")(d.singleholds_percent)
+           })
+           .attr("class", "avg_text")
+           .style("fill", color2);
+
+  }) // end on mouseover
+
+  hold_circles.on("mouseout", function(d) {
+    svg.select("#tooltip") //remove tooltip
+       .remove();
+
+    var currentElement = d3.select(this);
+    // Show averages again
+    hold_avg_text.style("fill", color1);
+    single_avg_text.style("fill", color2);
+    hold_avg_label.style("fill", "black");
+    single_avg_label.style("fill", "black");
+    hold_circles_avg.style("opacity", 1)
+                    .style("stroke-width", 4.5);
+    single_circles_avg.style("opacity", 1)
+                      .style("stroke-width", 4.5);
+
+    // Change opacity of mouseover circle
+    currentElement.style("opacity", .2);
+    single_circles.style("opacity", .2);
+
+  }) // end on mouseout
+
+ // Mouseover on single hold circles
+  single_circles.on("mouseover", function(d) {
+
+    var currentElement = d3.select(this);
+    var currentID = d.game_id;
+
+    // Determine starting x position
+    var xPosition;
+    if (d.division=="mens") {
+      xPosition = margin.left;
+    }
+    else if (d.division=="mixed") {
+      xPosition = margin.left + max_circle_r*2 + w_btwn_min;
+    }
+    else { xPosition = margin.left + max_circle_r*4 + w_btwn_min*2; }
+
+    // Hide averages
+    hold_avg_text.style("fill", "none");
+    single_avg_text.style("fill", "none");
+    hold_avg_label.style("fill", "none");
+    single_avg_label.style("fill", "none");
+    hold_circles_avg.style("opacity", .2);
+    single_circles_avg.style("opacity", .2);
+
+    // Change opacity of mouseover circle
+    currentElement.style("opacity", 1)
+                  .style("stroke-width", 5);
+    hold_circles.filter(function(d) {
+                  return d.game_id == currentID;
+                })
+                .style("opacity", 1)
+                .style("stroke-width", 5);
+
+    // Create a tooltip
+    var tooltip = svg.append("g")
+       .attr("id", "tooltip")
+       .attr("transform", "translate(" + xPosition + "," + (margin.top + 55 + max_circle_r*2) + ")")
+       .attr("width", max_circle_r*2)
+    // Add details
+    tooltip.append('text')
+           .attr("x", max_circle_r)
+           .attr("y", 5)
+           .text(function() {
+             return d.year + " " + d.tournament + " " + d.game_type;
+           })
+           .attr("class", "tooltip_text");
+    tooltip.append("text")
+           .attr("x", max_circle_r)
+           .attr("y", 18)
+           .text(function() {
+             return teamName_convert(d.win_team) + " (" + d.score.split("-")[0] + ") vs. " + teamName_convert(d.lose_team) + " (" + d.score.split("-")[1] + ")";
+           })
+           .attr("class", "tooltip_text");
+    tooltip.append("text")
+           .attr("x", max_circle_r)
+           .attr("y", 45)
+           .text(function() {
+             return d3.format(".0%")(d.holds_percent)
+           })
+           .attr("class", "avg_text")
+           .style("fill", color1);
+    tooltip.append("text")
+           .attr("x", max_circle_r)
+           .attr("y", 75)
+           .text(function() {
+             return d3.format(".0%")(d.singleholds_percent)
+           })
+           .attr("class", "avg_text")
+           .style("fill", color2);
+
+  }) // end on mouseover
+
+  single_circles.on("mouseout", function(d) {
+    svg.select("#tooltip") //remove tooltip
+       .remove();
+
+    var currentElement = d3.select(this);
+    // Show averages again
+    hold_avg_text.style("fill", color1);
+    single_avg_text.style("fill", color2);
+    hold_avg_label.style("fill", "black");
+    single_avg_label.style("fill", "black");
+    hold_circles_avg.style("opacity", 1)
+                    .style("stroke-width", 4.5);
+    single_circles_avg.style("opacity", 1)
+                      .style("stroke-width", 4.5);
+
+    // Change opacity of mouseover circle
+    currentElement.style("opacity", .2);
+    hold_circles.style("opacity", .2);
+
+  }) // end on mouseout
+
+// Transitions - enter
+  hold_circles.transition()
+              .duration(2000)
+              .delay(function(d,i) {
+                return i*30;
+              })
+              .style("stroke", color1);
+  hold_circles_avg.transition()
+                  .delay(1500)
+                  .duration(600)
+                  .style("opacity", 1)
+                  .style("stroke", color1);
+  hold_avg_label.transition()
+                .delay(2000)
+                .duration(1500)
+                .style('fill', 'black');
+  hold_avg_text.transition()
+                .delay(2000)
+                .duration(1500)
+                .style('fill', color1);
+  single_circles.transition()
+                .duration(2000)
+                .delay(function(d,i) {
+                  return 2000+i*30;
+                })
+                .style("stroke", color2);
+  single_circles_avg.transition()
+                    .delay(3500)
+                    .duration(600)
+                    .style("opacity", 1)
+                    .style("stroke", color2);
+  single_avg_label.transition()
+                  .delay(4000)
+                  .duration(1500)
+                  .style('fill', 'black');
+  single_avg_text.transition()
+                  .delay(4000)
+                  .duration(1500)
+                  .style('fill', color2);
 }) // end d3.csv
---
+++ js/utils.js
@@ -0,0 +1,40 @@
+// FUNCTIONS
+// text wrapping function
+function wrap(text, width) {
+  text.each(function () {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.3, // ems
+        x = text.attr("x"),
+        y = text.attr("y"),
+        dy = 0, //parseFloat(text.attr("dy")),
+        tspan = text.text(null)
+                    .append("tspan")
+                    .attr("x", x)
+                    .attr("y", y)
+                    .attr("dy", dy + "em");
+    while (word = words.pop()) {
+        line.push(word);
+        tspan.text(line.join(" "));
+        if (tspan.node().getComputedTextLength() > width) {
+            line.pop();
+            tspan.text(line.join(" "));
+            line = [word];
+            tspan = text.append("tspan")
+                        .attr("x", x)
+                        .attr("y", y)
+                        .attr("dy", ++lineNumber * lineHeight + dy + "em")
+                        .text(word);
+        }
+    }
+  });
+}; // end wrap function
+
+// Colors
+var teal = d3.rgb(56,118,104);
+var maroon = d3.rgb(170,62,71);
+var lime = d3.rgb(148,157,71);
+var orange = d3.rgb(234,158,43);
